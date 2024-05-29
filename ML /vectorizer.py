f13de419bbb39406e69ef077b0cffbf8a49a856e
Sravan-kotta <143123362+Sravan-kotta@users.noreply.github.com>
2024-05-29 14:14:27 +0530

    Delete ML  directory

--- ML /vectorizer.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-corpus = (file_path)
-
-vectorizer = TfidfVectorizer()
-
-X = vectorizer.fit_transform(corpus)
-
-print(vectorizer.get_feature_names_out())
